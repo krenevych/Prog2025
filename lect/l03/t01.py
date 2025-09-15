7eccac06e33b42ac2d7dff482e762319db70031a
Andrii Krenevych <krenevych@knu.ua>
2025-09-15 12:51:24 +0300

    lect 03

---
+++ lect/l03/t01.py
@@ -0,0 +1,16 @@
+
+# print("Hello, World")
+# print("Hello, World")
+# print("Hello, World")
+# print("Hello, World")
+# print("Hello, World")
+# print("Hello, World")
+# print("Hello, World")
+# print("Hello, World")
+# print("Hello, World")
+# print("Hello, World")
+
+# здійснити операцію print("Hello, World") 10 разів, або 20 разів
+
+for i in range(1000):
+    print("Hello, World")
