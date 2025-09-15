9c0b3fd343a6d453f73b86075dc318efc3676ed4
Andrii Krenevych <krenevych@knu.ua>
2025-09-15 09:23:47 +0300

    lect 03

---
+++ lect/l02/03.py
@@ -0,0 +1,16 @@
+a = 30
+b = 4
+c = 5
+
+exist = a + b > c and b + c > a and a + c > b
+
+n = 2 + 2 * 2 # 6 бо пріоритет арифметичних операцій
+
+a = True
+b = False
+c = True
+# condition = (a and (not b))
+# condition = (a or (b and c))
+
+print(exist)
+
