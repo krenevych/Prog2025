4732cb9f2458d8a3f2ad6c820559389e20cddff1
Andrii Krenevych <krenevych@knu.ua>
2025-09-24 10:50:59 +0300

    lab04

---
+++ labs/lab04/consult01.py
@@ -0,0 +1,10 @@
+# a = input()
+# b, c, d, e = a.split()
+# # a, b, c = [int(el) for el in input().split()]
+# pass
+
+a, b, c = map(int, input().split())
+print(a, b, c)
+
+
+
---
+++ labs/lab04/consult2.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+
+while n > 0:
+    d = n % 10
+    # print(d)
+    n = n // 10
+
+pass 
