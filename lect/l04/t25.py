7127f04a35bea1dd90e5e2b08570d548bb4324c7
Andrii Krenevych <krenevych@knu.ua>
2025-10-06 09:30:33 +0300

    topic 04

---
+++ lect/l04/t25.py
@@ -0,0 +1,13 @@
+tpl = (2, 3, 45, 34, 5, 11) # tuple - кортеж
+#      0  1   2   3  4   5
+
+# print(tpl[2])
+
+# for elem in tpl:
+#     print(elem)
+
+# print(len(tpl))
+# print(max(tpl))
+# print(tpl[2: ])
+
+# tpl[2] = 444 # зміна кортежу заборонена!
