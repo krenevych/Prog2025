22a744db4b6cdc494e859151dc2662c33d586fbb
Andrii Krenevych <krenevych@knu.ua>
2025-10-15 12:07:40 +0300

    lab06

---
+++ labs/lab06/t07.py
@@ -0,0 +1,10 @@
+while True:
+    # try:
+    #     s = input() # читаємо поточний рядок
+    # except:
+    #     break
+    words = input().split()
+    if len(words) == 0:
+        break
+
+    print(words)
