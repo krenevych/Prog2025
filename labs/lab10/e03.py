0acc38f07cd9e05361519bd1a30840784d624d33
Andrii Krenevych <krenevych@knu.ua>
2025-11-13 13:40:20 +0200

    lab10

---
+++ labs/lab10/e03.py
@@ -0,0 +1,25 @@
+# 1 3 2
+# 3 8 9
+# 1 9 0
+
+def read_matrix(N):
+    M = []
+    for i in range(N):
+        row = [int(el) for el in input().split()]
+        M.append(row)
+    return M
+
+def print_matrix(M, spaces=5):
+    for i in range(len(M)):  # i пробігає індекси рядків
+        for j in range(len(M[0])):
+            print(f"{M[i][j]:{spaces}}", end="")
+        print()
+    # for row in M:
+    #     print(row)
+
+
+
+N = int(input("Задайте кількість рядків матриці"))
+M = read_matrix(N)
+
+print_matrix(M, 10)
