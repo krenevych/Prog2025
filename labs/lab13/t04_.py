b028f2af6188d0439d6fb615ea6e6bc892d0b318
Andrii Krenevych <krenevych@knu.ua>
2025-11-26 12:11:40 +0200

    lab13

--- labs/lab13/t04_.py
+++ labs/lab13/t04_.py
@@ -29,8 +29,27 @@ def add_matrix(M1, M2):
         M.append(row)
     return M
 
+def mult_matrix(M1, M2):
+    assert len(M1[0]) == len(M2)
+
+    n = len(M1)
+    m = len(M2[0])
+
+    M = []
+    for i in range(n):
+        row = []
+        for j in range(m):
+            a = 0
+            for k in range(len(M2)):
+                a += M1[i][k] * M2[k][j]
+            row.append(a)
+        M.append(row)
+    return M
+
 if __name__ == '__main__':
     M1 = read_matrix("m1.txt")
     M2 = read_matrix("m2.txt")
-    M_sum = add_matrix(M1, M2)
-    write_matrix(M_sum, "sum.txt")
+    # M_sum = add_matrix(M1, M2)
+    M_mult = mult_matrix(M1, M2)
+    # write_matrix(M_sum, "sum.txt")
+    write_matrix(M_mult, "mult.txt")
